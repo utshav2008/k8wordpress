8d27fc14a3fb908b7cd804321d124853f35373df
Utsav Singh <utshav.singh@fireeye.com>
2018-10-02 00:55:57 +0530

    sed correction

--- wordpress/entrypoint.sh
+++ wordpress/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp -pr /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-ed -i -e "s/database_name_here/${DB_NAME}/" /var/www/html/wp-config.php
+sed -i -e "s/database_name_here/${DB_NAME}/" /var/www/html/wp-config.php
 sed -i -e "s/username_here/${DB_USER}/" /var/www/html/wp-config.php
 sed -i -e "s/password_here/${DB_PASSWORD}/" /var/www/html/wp-config.php
 sed -i -e "s/localhost/${DB_HOST}/" /var/www/html/wp-config.php
